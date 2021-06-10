1659c77f5612c872cd34013a267e1907e48cc5c7
venkataramana1191 <venkataramanab16@gmail.com>
2021-06-11 02:01:01 +0530

    sizes
    rendering lists done

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes : [7,8,9,10]
         }
     }
 })
